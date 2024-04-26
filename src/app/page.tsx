5ae0290a5c6302e7b00fd0a4250de6f8255eaf80
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-04-26 19:09:03 +0530

    small update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import Products from "@/components/Products";
 import { getProducts } from "@/helpers";
 import Image from "next/image";
 
+export const metadata = {
+  title: "ShopCart",
+  description: "E-Commerce App Using Nextjs",
+}
+
 export default async function Home() {
   // const products = await getProducts();
   // console.log(products);
